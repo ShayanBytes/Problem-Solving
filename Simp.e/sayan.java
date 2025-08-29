fb2ce1e16d30094d95cf7090395cbd0fec0e9e7b
ShayanBytes <shayan.corpservices@gmail.com>
2025-08-29 20:00:02 +0530

    .

--- Simp.e/sayan.java
+++ Simp.e/sayan.java
@@ -1,6 +1,12 @@
 
 public class sayan {
     public static void main(String[] args) {
-        System.out.println("Hello, Sayan!");
+        
+       int i = 1 ;
+     
+       while (int n <=10 ){
+        System.out.println(n*i);
+        n++;
+       }  
     }
 }
