ff32e48ffbaeb57af616f8779114cc7b906edd0f
ShayanBytes <shayan.corpservices@gmail.com>
2025-09-08 22:11:43 +0530

    .

---
+++ Strivers A2Z/1.3 Patterns/pattern3.java
@@ -0,0 +1,14 @@
+public class pattern3 {
+    public static void main (String[] args){
+        int n = 10;
+        for(int i=1;i<=10;i++){
+
+            for(int j=n-i;j>=1;j--){
+            System.out.print(" ");
+             } 
+            for(int k =1;k<=i;k++){
+                System.out.print("*");}
+            System.out.println();
+             }    
+    }
+}
